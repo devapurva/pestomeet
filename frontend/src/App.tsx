5d8369e2f4dd12199c2a8628e2cff869ce1cb1e0
Apurva Wadekar <apurva.w17@gmail.com>
2021-07-10 08:17:03 +0530

    updated material UI theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "./styles/theme";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <div>
+                <p>A bare bones application!</p>
+            </div>
+        </ThemeProvider>
+    );
 }
 
 export default App;
---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,54 @@
+import { createTheme } from "@material-ui/core/styles";
+import { red } from "@material-ui/core/colors";
+
+const theme = createTheme({
+    breakpoints: {
+        keys: ["xs", "sm", "sm", "lg", "xl"],
+        values: {
+            xs: 0,
+            sm: 600,
+            md: 960,
+            lg: 1280,
+            xl: 1920,
+        },
+    },
+    direction: "ltr",
+    mixins: {
+        toolbar: {
+            minHeight: 56,
+            "@media (min-width:0px) and (Screen.orientation: landscape)": {
+                minHeight: 48,
+            },
+            "@media (min-width:600px)": {
+                minHeight: 64,
+            },
+        },
+    },
+    palette: {
+        common: {
+            black: "#000",
+            white: "#fff",
+        },
+        type: "light",
+        primary: {
+            light: "#7986cb",
+            main: "#3f51b5",
+            dark: "#303f9f",
+            contrastText: "#fff",
+        },
+        secondary: {
+            light: "#ff4081",
+            main: "#f50057",
+            dark: "#c51162",
+            contrastText: "#fff",
+        },
+        error: {
+            light: "#e57373",
+            main: "#f44336",
+            dark: "#d32f2f",
+            contrastText: "#fff",
+        },
+    },
+});
+
+export default theme;
