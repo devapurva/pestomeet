39f88438e06544c079e9ca84ac9603d2cb9320a6
Apurva Wadekar <apurva.w17@gmail.com>
2021-07-30 15:14:46 +0530

    updated reducer

--- src/redux/slices/lists.ts
+++ src/redux/slices/lists.ts
@@ -1,21 +1,10 @@
-import { map } from 'lodash';
 import { createSlice } from '@reduxjs/toolkit';
 import { dispatch } from '../store';
 // utils
 import { HTTPClient } from '../../utils/axios';
 import {
-  Friend,
-  Gallery,
-  Profile,
-  UserPost,
-  Follower,
-  UserData,
-  CreditCard,
-  UserInvoice,
   UserManager,
   BatchManager,
-  UserAddressBook,
-  NotificationSettings,
   TeamManager,
   BatchMembers,
   TeamMember
@@ -26,9 +15,6 @@ import {
 type UserState = {
   isLoading: boolean;
   error: boolean;
-  myProfile: null | Profile;
-  posts: UserPost[];
-  users: UserData[];
   userList: UserManager[];
   studentInProgressList: UserManager[];
   studentApprovedList: UserManager[];
@@ -40,13 +26,6 @@ type UserState = {
   buddyList: TeamManager[];
   teamList: TeamManager[];
   resourceList: any[];
-  followers: Follower[];
-  friends: Friend[];
-  gallery: Gallery[];
-  cards: CreditCard[] | null;
-  addressBook: UserAddressBook[];
-  invoices: UserInvoice[];
-  notifications: NotificationSettings | null;
   metrics: {
     batchCount: number;
     studentCount: number;
@@ -58,9 +37,6 @@ type UserState = {
 const initialState: UserState = {
   isLoading: false,
   error: false,
-  myProfile: null,
-  posts: [],
-  users: [],
   userList: [],
   studentInProgressList: [],
   studentApprovedList: [],
@@ -72,13 +48,6 @@ const initialState: UserState = {
   buddyList: [],
   teamList: [],
   resourceList: [],
-  followers: [],
-  friends: [],
-  gallery: [],
-  cards: null,
-  addressBook: [],
-  invoices: [],
-  notifications: null,
   metrics: {
     batchCount: 0,
     studentCount: 0,
@@ -96,73 +65,10 @@ const slice = createSlice({
       state.isLoading = true;
     },
 
-    emptyUserList(state) {
-      state.users = [];
-    },
-
     // HAS ERROR
     hasError(state, action) {
       state.isLoading = false;
       state.error = action.payload;
-    },
-
-    // GET PROFILE
-    getProfileSuccess(state, action) {
-      state.isLoading = false;
-      state.myProfile = action.payload;
-    },
-
-    // GET POSTS
-    getPostsSuccess(state, action) {
-      state.isLoading = false;
-      state.posts = action.payload;
-    },
-
-    // GET USERS
-    getUsersSuccess(state, action) {
-      state.isLoading = false;
-      state.users = action.payload;
-    },
-
-    // DELETE USERS
-    // deleteUser(state, action) {
-    //   const deleteUser = filter(state.userList, (user) => user.id !== action.payload);
-    //   state.userList = deleteUser;
-    // },
-
-    // GET FOLLOWERS
-    getFollowersSuccess(state, action) {
-      state.isLoading = false;
-      state.followers = action.payload;
-    },
-
-    // ON TOGGLE FOLLOW
-    onToggleFollow(state, action) {
-      const followerId = action.payload;
-
-      const handleToggle = map(state.followers, (follower) => {
-        if (follower.id === followerId) {
-          return {
-            ...follower,
-            isFollowed: !follower.isFollowed
-          };
-        }
-        return follower;
-      });
-
-      state.followers = handleToggle;
-    },
-
-    // GET FRIENDS
-    getFriendsSuccess(state, action) {
-      state.isLoading = false;
-      state.friends = action.payload;
-    },
-
-    // GET GALLERY
-    getGallerySuccess(state, action) {
-      state.isLoading = false;
-      state.gallery = action.payload;
     },
 
     // GET METRICS
@@ -229,109 +135,12 @@ const slice = createSlice({
     getResourceListSuccess(state, action) {
       state.isLoading = false;
       state.resourceList = action.payload;
-    },
-
-    // GET CARDS
-    getCardsSuccess(state, action) {
-      state.isLoading = false;
-      state.cards = action.payload;
-    },
-
-    // GET ADDRESS BOOK
-    getAddressBookSuccess(state, action) {
-      state.isLoading = false;
-      state.addressBook = action.payload;
-    },
-
-    // GET INVOICES
-    getInvoicesSuccess(state, action) {
-      state.isLoading = false;
-      state.invoices = action.payload;
-    },
-
-    // GET NOTIFICATIONS
-    getNotificationsSuccess(state, action) {
-      state.isLoading = false;
-      state.notifications = action.payload;
     }
   }
 });
 
 // Reducer
 export default slice.reducer;
-
-// Actions
-export const { onToggleFollow } = slice.actions;
-
-// ----------------------------------------------------------------------
-
-export function getProfile() {
-  return async () => {
-    dispatch(slice.actions.startLoading());
-    try {
-      const response = await HTTPClient.get('/api/user/profile');
-      dispatch(slice.actions.getProfileSuccess(response.data.profile));
-    } catch (error) {
-      dispatch(slice.actions.hasError(error));
-    }
-  };
-}
-
-// ----------------------------------------------------------------------
-
-export function getPosts() {
-  return async () => {
-    dispatch(slice.actions.startLoading());
-    try {
-      const response = await HTTPClient.get('/api/user/posts');
-      dispatch(slice.actions.getPostsSuccess(response.data.posts));
-    } catch (error) {
-      dispatch(slice.actions.hasError(error));
-    }
-  };
-}
-
-// ----------------------------------------------------------------------
-
-export function getFollowers() {
-  return async () => {
-    dispatch(slice.actions.startLoading());
-    try {
-      const response = await HTTPClient.get('/api/user/social/followers');
-      dispatch(slice.actions.getFollowersSuccess(response.data.followers));
-    } catch (error) {
-      dispatch(slice.actions.hasError(error));
-    }
-  };
-}
-
-// ----------------------------------------------------------------------
-
-export function getFriends() {
-  return async () => {
-    dispatch(slice.actions.startLoading());
-    try {
-      const response = await HTTPClient.get('/api/user/social/friends');
-      dispatch(slice.actions.getFriendsSuccess(response.data.friends));
-    } catch (error) {
-      dispatch(slice.actions.hasError(error));
-    }
-  };
-}
-
-// ----------------------------------------------------------------------
-
-export function getGallery() {
-  return async () => {
-    dispatch(slice.actions.startLoading());
-    try {
-      const response = await HTTPClient.get('/api/user/social/gallery');
-      dispatch(slice.actions.getGallerySuccess(response.data.gallery));
-    } catch (error) {
-      dispatch(slice.actions.hasError(error));
-    }
-  };
-}
 
 // ----------------------------------------------------------------------
 
@@ -475,77 +284,6 @@ export const deleteTeam = async (id: string) => {
 
 // ----------------------------------------------------------------------
 
-export function getCards() {
-  return async () => {
-    dispatch(slice.actions.startLoading());
-    try {
-      const response = await HTTPClient.get('/api/user/account/cards');
-      dispatch(slice.actions.getCardsSuccess(response.data.cards));
-    } catch (error) {
-      dispatch(slice.actions.hasError(error));
-    }
-  };
-}
-
-// ----------------------------------------------------------------------
-
-export function getAddressBook() {
-  return async () => {
-    dispatch(slice.actions.startLoading());
-    try {
-      const response = await HTTPClient.get('/api/user/account/address-book');
-      dispatch(slice.actions.getAddressBookSuccess(response.data.addressBook));
-    } catch (error) {
-      dispatch(slice.actions.hasError(error));
-    }
-  };
-}
-
-// ----------------------------------------------------------------------
-
-export function getInvoices() {
-  return async () => {
-    dispatch(slice.actions.startLoading());
-    try {
-      const response = await HTTPClient.get('/api/user/account/invoices');
-      dispatch(slice.actions.getInvoicesSuccess(response.data.invoices));
-    } catch (error) {
-      dispatch(slice.actions.hasError(error));
-    }
-  };
-}
-
-// ----------------------------------------------------------------------
-
-export function getNotifications() {
-  return async () => {
-    dispatch(slice.actions.startLoading());
-    try {
-      const response = await HTTPClient.get('/api/user/account/notifications-settings');
-      dispatch(slice.actions.getNotificationsSuccess(response.data.notifications));
-    } catch (error) {
-      dispatch(slice.actions.hasError(error));
-    }
-  };
-}
-
-// ----------------------------------------------------------------------
-
-export function getUsers() {
-  return async () => {
-    dispatch(slice.actions.startLoading());
-    dispatch(slice.actions.emptyUserList());
-    try {
-      const response = await HTTPClient.get('/api/user/all');
-      dispatch(slice.actions.getUsersSuccess(response.data.users));
-    } catch (error) {
-      dispatch(slice.actions.hasError(error));
-    }
-  };
-}
-
-// ----------------------------------------------------------------------
-
 export const addUser = async (
   name: string,
   role: string,
@@ -604,16 +342,6 @@ export const editUser = async (
     email,
     approval
   });
-  if (response.data.statusCode) {
-    const user = {
-      name,
-      role,
-      phone,
-      experience,
-      email,
-      approval
-    };
-  }
   return response;
 };
 
