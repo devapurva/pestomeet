1647d337828ec0b4117b2f49209adef6c7e82d95
Apurva Wadekar <apurva.w17@gmail.com>
2021-08-04 21:06:16 +0530

    authentication testing completed

--- cypress/integration/authentication/register_spec.js
+++ cypress/integration/authentication/register_spec.js
@@ -1,17 +1,23 @@
 /// <reference types="cypress" />
 
 describe('Resgiter to App', () => {
-  it('Login without any inputs', () => {
-    cy.visit('http://localhost:3000/auth/resgiter');
+  it('Route to Register Page', () => {
+    cy.visit('http://localhost:3000/auth/register');
     cy.get('form').submit();
   });
-  //   it('Without Login Visit Dashboard', () => {
-  //     cy.visit('http://localhost:3000/dashboard');
-  //   });
-  //   it('Login with input', () => {
-  //     cy.visit('http://localhost:3000/');
-  //     cy.get('input[type=email]').type('ninja4epsilon@gmail.com');
-  //     cy.get('input[type=password]').type('Abc!23456');
-  //     cy.get('form').submit();
-  //   });
+  it('Submit Registration without Inputs', () => {
+    cy.visit('http://localhost:3000/auth/register');
+    cy.get('form').submit();
+  });
+  it('Submit Registration with Inputs', () => {
+    cy.visit('http://localhost:3000/auth/register');
+    cy.get('input[name=name]').type('Test Student One');
+    cy.get('input[type=email]').type('testStudent@student.com');
+    cy.get('input[name=phone]').type('8738272182');
+    cy.get('[type="radio"]').first().check();
+    cy.get('input[name=experience]').type('5');
+    cy.get('input[name=password]').type('Abc!23456');
+    cy.get('input[name=confirmPassword]').type('Abc!23456');
+    cy.get('form').submit();
+  });
 });
