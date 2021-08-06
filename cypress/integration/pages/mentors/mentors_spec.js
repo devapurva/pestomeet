b2328d2af5f21a9e9c20ee599189293ae7f606ab
Apurva Wadekar <apurva.w17@gmail.com>
2021-08-06 19:41:08 +0530

    add user, add student, add mentor completed

--- cypress/integration/pages/mentors/mentors_spec.js
+++ cypress/integration/pages/mentors/mentors_spec.js
@@ -1,25 +1,6 @@
 /// <reference types="cypress" />
 
-describe('All Users Modules', () => {
-  it('Add User: Successfully', () => {
-    cy.visit('http://localhost:3000/');
-    cy.get('input[type=email]').type('ninja4epsilon@gmail.com');
-    cy.get('input[type=password]').type('Abc!23456');
-    cy.get('form').submit();
-    cy.url()
-      .should('include', '/dashboard/overview')
-      .then(() => {
-        cy.visit('http://localhost:3000/dashboard/all-user');
-        cy.get('button[id="add-user"]').click();
-        cy.get('input[name=name]').type('Test Student Two');
-        cy.get('input[name=email]').type('testStudenttwo@student.com');
-        cy.get('input[name=phone]').type('8438232182');
-        cy.get('[type="radio"]').check('student');
-        cy.get('input[name=experience]').type('5');
-        cy.get('input[name=password]').type('Abc!23456');
-        cy.get('form').submit();
-      });
-  });
+describe('Mentors Modules', () => {
   it('Show Validation Errors', () => {
     cy.visit('http://localhost:3000/');
     cy.get('input[type=email]').type('ninja4epsilon@gmail.com');
@@ -28,11 +9,27 @@ describe('All Users Modules', () => {
     cy.url()
       .should('include', '/dashboard/overview')
       .then(() => {
-        cy.visit('http://localhost:3000/dashboard/all-user');
+        cy.visit('http://localhost:3000/dashboard/mentor');
         cy.get('button[id="add-user"]').click();
-        cy.get('input[name=name]').type('Test Student Three');
-        cy.get('input[name=phone]').type('8438632182');
-        cy.get('[type="radio"]').check('student');
+        cy.get('input[name=name]').type('Test Mentor Two');
+        cy.get('input[name=phone]').type('7362718291');
+        cy.get('form').submit();
+      });
+  });
+  it('Add Mentor: Successfully', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('input[type=email]').type('ninja4epsilon@gmail.com');
+    cy.get('input[type=password]').type('Abc!23456');
+    cy.get('form').submit();
+    cy.url()
+      .should('include', '/dashboard/overview')
+      .then(() => {
+        cy.visit('http://localhost:3000/dashboard/mentor');
+        cy.get('button[id="add-user"]').click();
+        cy.get('input[name=name]').type('Test Mentor Three');
+        cy.get('input[name=email]').type('testmentorthree@mentor.com');
+        cy.get('input[name=phone]').type('7362718540');
+        cy.get('input[name=password]').type('Abc!23456');
         cy.get('form').submit();
       });
   });
